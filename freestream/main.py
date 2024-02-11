9d83aa6481bda511184d5ae0be9aaae192b96f98
Daethyra <109057945+Daethyra@users.noreply.github.com>
2024-02-10 23:21:57 -0800

    ran black

--- freestream/main.py
+++ freestream/main.py
@@ -4,7 +4,12 @@ from langchain_openai import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain_community.chat_message_histories import StreamlitChatMessageHistory
 from langchain.chains import ConversationalRetrievalChain
-from utility_funcs import configure_retriever, StreamHandler, PrintRetrievalHandler, set_llm
+from utility_funcs import (
+    configure_retriever,
+    StreamHandler,
+    PrintRetrievalHandler,
+    set_llm,
+)
 
 # Initialize LangSmith tracing
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
@@ -24,7 +29,7 @@ uploaded_files = st.sidebar.file_uploader(
     label="Upload a PDF or text file",
     type=["pdf", "doc", "docx", "txt"],
     help="Types supported: pdf, doc, docx, txt",
-    accept_multiple_files=True
+    accept_multiple_files=True,
 )
 if not uploaded_files:
     st.info("Please upload documents to continue.")
@@ -34,41 +39,41 @@ retriever = configure_retriever(uploaded_files)
 
 # Setup memory for contextual conversation
 msgs = StreamlitChatMessageHistory()
-memory = ConversationBufferMemory(memory_key="chat_history", chat_memory=msgs, return_messages=True)
+memory = ConversationBufferMemory(
+    memory_key="chat_history", chat_memory=msgs, return_messages=True
+)
 
 # Create a dictionary with keys to chat model classes
 model_names = {
-   "ChatOpenAI GPT-3.5 Turbo": ChatOpenAI( # Define a dictionary entry for the "ChatOpenAI GPT-3.5 Turbo" model
-       model_name="gpt-3.5-turbo-0125",  # Set the OpenAI model name
-       openai_api_key=st.secrets.OPENAI.openai_api_key,  # Set the OpenAI API key from the Streamlit secrets manager
-       temperature=0.7,  # Set the temperature for the model's responses
-       streaming=True  # Enable streaming responses for the model
-   ),
+    "ChatOpenAI GPT-3.5 Turbo": ChatOpenAI(  # Define a dictionary entry for the "ChatOpenAI GPT-3.5 Turbo" model
+        model_name="gpt-3.5-turbo-0125",  # Set the OpenAI model name
+        openai_api_key=st.secrets.OPENAI.openai_api_key,  # Set the OpenAI API key from the Streamlit secrets manager
+        temperature=0.7,  # Set the temperature for the model's responses
+        streaming=True,  # Enable streaming responses for the model
+    ),
 }
 
 # Create a dropdown menu for selecting a chat model
 selected_model = st.selectbox(
-   label="Choose your chat model:",  # Set the label for the dropdown menu
-   options=list(model_names.keys()),  # Set the available model options
-   key="model_selector",  # Set a unique key for the dropdown menu
-   on_change=set_llm  # Set the callback function
+    label="Choose your chat model:",  # Set the label for the dropdown menu
+    options=list(model_names.keys()),  # Set the available model options
+    key="model_selector",  # Set a unique key for the dropdown menu
+    on_change=set_llm,  # Set the callback function
 )
 
 # Load the selected model dynamically
-llm = model_names[selected_model]  # Get the selected model \
-    # from the `model_names` dictionary
+llm = model_names[
+    selected_model
+]  # Get the selected model from the `model_names` dictionary
 
 # Create a chain that ties everything together
 qa_chain = ConversationalRetrievalChain.from_llm(
-    llm,
-    retriever=retriever,
-    memory=memory,
-    verbose=True
+    llm, retriever=retriever, memory=memory, verbose=True
 )
 
 # if the length of messages is 0, or when the user \
-    # clicks the clear button,
-    # show a default message from the AI
+# clicks the clear button,
+# show a default message from the AI
 if len(msgs.messages) == 0 or st.sidebar.button("Clear message history"):
     msgs.clear()
     # show a default message from the AI
@@ -82,14 +87,16 @@ for msg in msgs.messages:
 # Display user input field and enter button
 if user_query := st.chat_input(placeholder="Ask me anything!"):
     st.chat_message("user").write(user_query)
-            
+
     # Display assistant response
     with st.chat_message("assistant"):
         # Check for the presence of the "messages" key in session state
-        if 'messages' not in st.session_state:
+        if "messages" not in st.session_state:
             st.session_state.messages = []
 
         retrieval_handler = PrintRetrievalHandler(st.container())
         stream_handler = StreamHandler(st.empty())
-        response = qa_chain.run(user_query, callbacks=[retrieval_handler, stream_handler])
-        st.toast('Success!', icon="✅")
+        response = qa_chain.run(
+            user_query, callbacks=[retrieval_handler, stream_handler]
+        )
+        st.toast("Success!", icon="✅")
