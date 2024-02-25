1257e1bb38c368b53373057465c0184055fa5ba3
Daethyra <109057945+Daethyra@users.noreply.github.com>
2024-02-25 14:32:18 -0800

    feat(pages): add RAGbot page and utility functions
    - Added a new file "RAGbot.py" to the freestream/pages directory, which includes the implementation of a chatbot assistant using LangSmith tracing.
    - Moved utility functions in the "utility_funcs.py" file for configuring and returning a retriever object for a given list of uploaded files, as well as callback handlers for streaming the model's output to the user interface and printing the context retrieval status.

---
+++ freestream/pages/RAGbot.py
@@ -0,0 +1,114 @@
+import os
+import streamlit as st
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_openai import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain_community.chat_message_histories import StreamlitChatMessageHistory
+from langchain.chains import ConversationalRetrievalChain
+from pages.utils.utility_funcs import (
+    configure_retriever,
+    StreamHandler,
+    PrintRetrievalHandler,
+    set_llm,
+)
+
+# Initialize LangSmith tracing
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_PROJECT"] = "FreeStream-v2.0.2"
+os.environ["LANGCHAIN_ENDPOINT"] = st.secrets.LANGCHAIN.LANGCHAIN_ENDPOINT
+os.environ["LANGCHAIN_API_KEY"] = st.secrets.LANGCHAIN.LANGCHAIN_API_KEY
+
+# Set up page config
+st.set_page_config(page_title="FreeStream: ", page_icon="🗣️📄")
+st.title("FreeStream")
+st.header(":green[_Welcome_]", divider="red")
+st.caption(":violet[_General purpose chatbot assistant_]")
+st.sidebar.subheader("__User Panel__")
+
+# Add a way to upload files
+uploaded_files = st.sidebar.file_uploader(
+    label="Upload a PDF or text file",
+    type=["pdf", "doc", "docx", "txt"],
+    help="Types supported: pdf, doc, docx, txt",
+    accept_multiple_files=True,
+)
+if not uploaded_files:
+    st.info("Please upload documents to continue.")
+    st.stop()
+
+retriever = configure_retriever(uploaded_files)
+
+# Setup memory for contextual conversation
+msgs = StreamlitChatMessageHistory()
+memory = ConversationBufferMemory(
+    memory_key="chat_history", chat_memory=msgs, return_messages=True
+)
+
+# Create a dictionary with keys to chat model classes
+model_names = {
+    "GPT-3.5 Turbo": ChatOpenAI(  # Define a dictionary entry for the "ChatOpenAI GPT-3.5 Turbo" model
+        model_name="gpt-3.5-turbo-0125",  # Set the OpenAI model name
+        openai_api_key=st.secrets.OPENAI.openai_api_key,  # Set the OpenAI API key from the Streamlit secrets manager
+        temperature=0.7,  # Set the temperature for the model's responses
+        streaming=True,  # Enable streaming responses for the model
+    ),
+    
+    "Gemini-Pro": ChatGoogleGenerativeAI(
+        model="gemini-pro",
+        google_api_key=st.secrets.GOOGLE.google_api_key,
+        temperature=0.5,
+        top_k=40,
+        top_p=0.65,
+        convert_system_message_to_human=True,
+    )
+}
+
+# Create a dropdown menu for selecting a chat model
+selected_model = st.selectbox(
+    label="Choose your chat model:",  # Set the label for the dropdown menu
+    options=list(model_names.keys()),  # Set the available model options
+    key="model_selector",  # Set a unique key for the dropdown menu
+    on_change=lambda: set_llm(st.session_state.model_selector, model_names),  # Set the callback function
+)
+
+# Load the selected model dynamically
+llm = model_names[
+    selected_model
+]  # Get the selected model from the `model_names` dictionary
+
+# Create a chain that ties everything together
+qa_chain = ConversationalRetrievalChain.from_llm(
+    llm, retriever=retriever, memory=memory, verbose=True
+)
+
+# if the length of messages is 0, or when the user \
+# clicks the clear button,
+# show a default message from the AI
+if len(msgs.messages) == 0 or st.sidebar.button("Clear message history"):
+    msgs.clear()
+    # show a default message from the AI
+    msgs.add_ai_message("How can I help you?")
+
+# Display coversation history window
+avatars = {"human": "user", "ai": "assistant"}
+for msg in msgs.messages:
+    st.chat_message(avatars[msg.type]).write(msg.content)
+
+# Display user input field and enter button
+if user_query := st.chat_input(placeholder="Ask me anything!"):
+    st.chat_message("user").write(user_query)
+
+    # Display assistant response
+    with st.chat_message("assistant"):
+        # Check for the presence of the "messages" key in session state
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
+
+        retrieval_handler = PrintRetrievalHandler(st.container())
+        stream_handler = StreamHandler(st.empty())
+        response = qa_chain.run(
+            user_query, callbacks=[retrieval_handler, stream_handler]
+        )
+        if selected_model == "Gemini-Pro":
+            st.write(response)
+        st.toast("Success!", icon="✅")
