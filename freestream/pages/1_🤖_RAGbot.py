520fd74d428f2e25943bd8d9ea257090c70c31a6
Daethyra <109057945+Daethyra@users.noreply.github.com>
2024-03-17 16:26:36 -0700

    chore(freestream): reorganize chat model imports
    Reorganized the import statements for chat models in "1_🗦_RAGbot.py" to improve readability.

--- freestream/pages/1_🤖_RAGbot.py
+++ freestream/pages/1_🤖_RAGbot.py
@@ -3,18 +3,18 @@ import os
 import streamlit as st
 from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
+from langchain_anthropic import ChatAnthropic
 from langchain_community.chat_message_histories import StreamlitChatMessageHistory
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_anthropic import ChatAnthropic
 from langchain_openai import ChatOpenAI
-from pages.utils.utility_funcs import (
+from pages.utils import (
     PrintRetrievalHandler,
-    StreamHandler,
     RetrieveDocuments,
+    StreamHandler,
+    chat_models,
+    footer,
     set_llm,
-    clear_messages,
 )
-from pages.utils.styles import footer
 
 # Initialize LangSmith tracing
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
@@ -66,57 +66,6 @@ msgs = StreamlitChatMessageHistory()
 memory = ConversationBufferMemory(
     memory_key="chat_history", chat_memory=msgs, return_messages=True
 )
-
-# Create a dictionary with keys to chat model classes
-model_names = {
-    "GPT-3.5 Turbo": ChatOpenAI(  # Define a dictionary entry for the "ChatOpenAI GPT-3.5 Turbo" model
-        model="gpt-3.5-turbo-0125",  # Set the OpenAI model name
-        openai_api_key=st.secrets.OPENAI.openai_api_key,  # Set the OpenAI API key from the Streamlit secrets manager
-        temperature=temperature_slider,  # Set the temperature for the model's responses using the sidebar slider
-        streaming=True,  # Enable streaming responses for the model
-        max_tokens=4096,  # Set the maximum number of tokens for the model's responses
-        max_retries=1,  # Set the maximum number of retries for the model
-    ),
-    "GPT-4 Turbo": ChatOpenAI(
-        model="gpt-4-0125-preview",
-        openai_api_key=st.secrets.OPENAI.openai_api_key,
-        temperature=temperature_slider,
-        streaming=True,
-        max_tokens=4096,
-        max_retries=1,
-    ),
-    "Claude: Haiku": ChatAnthropic(
-        model="claude-3-haiku-20240307",
-        anthropic_api_key=st.secrets.ANTHROPIC.anthropic_api_key,
-        temperature=temperature_slider,
-        streaming=True,
-        max_tokens=4096,
-    ),
-    "Claude: Sonnet": ChatAnthropic(
-        model="claude-3-sonnet-20240229",
-        anthropic_api_key=st.secrets.ANTHROPIC.anthropic_api_key,
-        temperature=temperature_slider,
-        streaming=True,
-        max_tokens=4096,
-    ),
-    "Claude: Opus": ChatAnthropic(
-        model="claude-3-opus-20240229",
-        anthropic_api_key=st.secrets.ANTHROPIC.anthropic_api_key,
-        temperature=temperature_slider,
-        streaming=True,
-        max_tokens=4096,
-    ),
-    "Gemini-Pro": ChatGoogleGenerativeAI(
-        model="gemini-pro",
-        google_api_key=st.secrets.GOOGLE.google_api_key,
-        temperature=temperature_slider,
-        top_k=50,
-        top_p=0.7,
-        convert_system_message_to_human=True,
-        max_output_tokens=4096,
-        max_retries=1,
-    ),
-}
 
 # Create a dropdown menu for selecting a chat model
 selected_model = st.selectbox(
