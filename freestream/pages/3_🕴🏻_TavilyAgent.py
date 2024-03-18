c67e103825a3e6cc46efcf1b5455e9cb15f0a2c7
Daethyra <109057945+Daethyra@users.noreply.github.com>
2024-03-17 21:00:52 -0700

    chore(freestream): refactor chat model configuration
    - Refactored the chat model configuration to use a dictionary with keys representing chat model classes. This change allows for easier management and addition of new chat models in the future. Also, added a dropdown menu for selecting a chat model and set up the Streamlit callback handler accordingly.

--- freestream/pages/3_🕴🏻_TavilyAgent.py
+++ freestream/pages/3_🕴🏻_TavilyAgent.py
@@ -9,8 +9,7 @@ from langchain_community.callbacks.streamlit import StreamlitCallbackHandler
 from langchain_community.chat_message_histories import \
     StreamlitChatMessageHistory
 from langchain_community.tools.tavily_search import TavilySearchResults
-from langchain_openai import ChatOpenAI
-from pages.utils import chat_models, footer
+from pages.utils import footer
 
 # Initialize LangSmith tracing
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
@@ -25,23 +24,61 @@ st.set_page_config(page_title="TavilySearch Chatbot")
 # Show footer
 st.markdown(footer, unsafe_allow_html=True)
 
-# Create a dropdown menu for selecting a chat model
-selected_model = st.selectbox(
-    label="Choose your chat model:",  # Set the label for the dropdown menu
-    options=list(model_names.keys()),  # Set the available model options
-    key="model_selector",  # Set a unique key for the dropdown menu
-    on_change=lambda: set_llm(
-        st.session_state.model_selector, model_names
-    ),  # Set the callback function
-)
-
-# Set up Streamlit callback handler
-st_callback = StreamlitCallbackHandler(st.container())
-
 ### ----------------------------------------------------- ###
 
 
 ### LLM Peripherals setup ###
+# Create a dictionary with keys to chat model classes
+model_names = {
+    "GPT-3.5 Turbo": ChatOpenAI(  # Define a dictionary entry for the "ChatOpenAI GPT-3.5 Turbo" model
+        model="gpt-3.5-turbo-0125",  # Set the OpenAI model name
+        openai_api_key=st.secrets.OPENAI.openai_api_key,  # Set the OpenAI API key from the Streamlit secrets manager
+        temperature=temperature_slider,  # Set the temperature for the model's responses using the sidebar slider
+        streaming=True,  # Enable streaming responses for the model
+        max_tokens=4096,  # Set the maximum number of tokens for the model's responses
+        max_retries=1,  # Set the maximum number of retries for the model
+    ),
+    "GPT-4 Turbo": ChatOpenAI(
+        model="gpt-4-0125-preview",
+        openai_api_key=st.secrets.OPENAI.openai_api_key,
+        temperature=temperature_slider,
+        streaming=True,
+        max_tokens=4096,
+        max_retries=1,
+    ),
+    "Claude: Haiku": ChatAnthropic(
+        model="claude-3-haiku-20240307",
+        anthropic_api_key=st.secrets.ANTHROPIC.anthropic_api_key,
+        temperature=temperature_slider,
+        streaming=True,
+        max_tokens=4096,
+    ),
+    "Claude: Sonnet": ChatAnthropic(
+        model="claude-3-sonnet-20240229",
+        anthropic_api_key=st.secrets.ANTHROPIC.anthropic_api_key,
+        temperature=temperature_slider,
+        streaming=True,
+        max_tokens=4096,
+    ),
+    "Claude: Opus": ChatAnthropic(
+        model="claude-3-opus-20240229",
+        anthropic_api_key=st.secrets.ANTHROPIC.anthropic_api_key,
+        temperature=temperature_slider,
+        streaming=True,
+        max_tokens=4096,
+    ),
+    "Gemini-Pro": ChatGoogleGenerativeAI(
+        model="gemini-pro",
+        google_api_key=st.secrets.GOOGLE.google_api_key,
+        temperature=temperature_slider,
+        top_k=50,
+        top_p=0.7,
+        convert_system_message_to_human=True,
+        max_output_tokens=4096,
+        max_retries=1,
+    ),
+}
+
 # Set up memory for contextual conversation
 msgs = StreamlitChatMessageHistory()
 memory = ConversationBufferMemory(
@@ -62,6 +99,21 @@ toolbox = [tavily_search]
 ## TOOLS ##
 
 ### ----------------------------------------------------- ###
+
+### Chatbot Configuration Widgets ###
+# Create a dropdown menu for selecting a chat model
+selected_model = st.selectbox(
+    label="Choose your chat model:",  # Set the label for the dropdown menu
+    options=list(model_names.keys()),  # Set the available model options
+    key="model_selector",  # Set a unique key for the dropdown menu
+    on_change=lambda: set_llm(
+        st.session_state.model_selector, model_names
+    ),  # Set the callback function
+)
+
+# Set up Streamlit callback handler
+st_callback = StreamlitCallbackHandler(st.container())
+
 
 
 ### Agent Configuration ###
